ebe7fb44e07b6db0bb40fbcff1c04a36d9d51934
Ashu <ashutoshm90@gmail.com>
2015-10-21 11:12:00 +0530

    first commit

---
+++ ashu/urls.py
@@ -0,0 +1,46 @@
+﻿"""
+Definition of urls for ashu.
+"""
+
+from datetime import datetime
+from django.conf.urls import patterns, url
+from app.forms import BootstrapAuthenticationForm
+
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^artist/create$', 'app.views.artistscreate', name= 'artistcreate'),
+    url(r'^artists$', 'app.views.artists', name = 'artists'),
+    url(r'^artist/(?P<id>\d+)$', 'app.views.artistdetails', name = 'artistdetails'),
+    # Examples:
+    url(r'^$', 'app.views.home', name='home'),
+    url(r'^contact$', 'app.views.contact', name='contact'),
+    url(r'^about', 'app.views.about', name='about'),
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Log in',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        'django.contrib.auth.views.logout',
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
